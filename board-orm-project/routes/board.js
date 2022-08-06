918d216e3ac4cbaeece069fc7fa8d4e008ee98d1
YUZAMIN <untmd.crmn@gmail.com>
2022-08-07 01:26:46 +0900

    :sparkles: mock data 추가

--- board-orm-project/routes/board.js
+++ board-orm-project/routes/board.js
@@ -2,8 +2,8 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/list", function (req, res, next) {
-  res.render("board/list", { layout: "layout" });
+router.get("/list", async (req, res, next) => {
+  res.render("board/list");
 });
 
 module.exports = router;
