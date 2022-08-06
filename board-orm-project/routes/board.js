dc74d67d8a365de4fabad4669e1f532a78563525
YUZAMIN <untmd.crmn@gmail.com>
2022-08-07 01:39:12 +0900

    :sparkles: 게시글 작성 view 파일 생성 및 routing 구현

--- board-orm-project/routes/board.js
+++ board-orm-project/routes/board.js
@@ -2,8 +2,14 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/list", async (req, res, next) => {
+router.get("/list", async (req, res) => {
   res.render("board/list");
 });
+
+router.get("/create", async (req, res) => {
+  res.render("board/create");
+});
+
+router.post("/create", async (req, res) => {});
 
 module.exports = router;
