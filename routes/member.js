817ef34b097b3d281ac315f8af22e295d567f790
YUZAMIN <untmd.crmn@gmail.com>
2022-08-19 01:17:49 +0900

    :sparkles: members 테이블에 회원 정보 추가 routing 구현

--- routes/member.js
+++ routes/member.js
@@ -1,9 +1,22 @@
 var express = require("express");
 const db = require("../models");
 var router = express.Router();
+const bcrypt = require("bcryptjs");
 
 router.get("/signUp", async (req, res, next) => {
   res.render("member/signUp");
+});
+
+router.post("/signUp", async (req, res, next) => {
+  const encryptedPassword = await bcrypt.hash(req.body.userPassword, 12);
+  const memberData = {
+    user_id: req.body.userId,
+    user_password: encryptedPassword,
+    user_name: req.body.userName,
+    user_phone: req.body.userPhone,
+  };
+  await db.Member.create(memberData);
+  res.redirect("/board/list");
 });
 
 router.get("/signIn", async (req, res, next) => {
