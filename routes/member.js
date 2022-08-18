ce4b86b88475e323ffa2859a4f248f053d85fb79
YUZAMIN <untmd.crmn@gmail.com>
2022-08-19 00:49:24 +0900

    :sparkles: 회원가입 페이지 routing 구현

--- routes/member.js
+++ routes/member.js
@@ -2,6 +2,10 @@ var express = require("express");
 const db = require("../models");
 var router = express.Router();
 
+router.get("/signUp", async (req, res, next) => {
+  res.render("member/signUp");
+});
+
 router.get("/signIn", async (req, res, next) => {
   res.render("member/signIn", { signInResult: "" });
 });
